7410626caf1c7eaeacee830f01e09f21866a13c1
guifabre <guijoseph.fabre@gmail.com>
2023-11-09 13:46:27 -0500

    update web site : - rewriting of the page data processing elements - suppression of initiate data processing - add labels to functions exported from madshapR in references - autommatic updates for the package itself

--- update_files.R
+++ update_files.R
@@ -4,14 +4,25 @@ library(Rmonize)
 # devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
 # devtools::build_readme()
 # devtools::build_rmd('NEWS.Rmd')
+
 fs::dir_delete("docs")
 pkgdown::build_site()
+
+fs::file_move(
+  "../Rmonize-documentation/docs/reference/index.html",
+  "../Rmonize-documentation/index.html")
 
 fs::dir_delete("../Rmonize-documentation/docs")
 fs::dir_copy(
   "../Rmonize/docs",
   "../Rmonize-documentation/docs",
   overwrite = TRUE)
+
+fs::file_move(
+  "../Rmonize-documentation/index.html",
+  "../Rmonize-documentation/docs/reference/index.html")
+
+file.edit("../Rmonize-documentation/docs/reference/index.html")
 
 # switch to documentation
 source('template_script.R')
